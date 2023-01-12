5660352063c25e6dc2810a5aead704016801f513
Vladislav <dubovecvlad0@gmail.com>
2023-01-12 11:10:19 +0300

    homepage in blog app

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
 
-def home(request):
-    return render(request, 'blog/home.html')
+class Home(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Classic Blog Design'
+        return context
