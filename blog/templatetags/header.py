cb1683cfc432ed844a3d224c7948593ad42a6155
Vladislav <dubovecvlad0@gmail.com>
2023-01-12 17:06:20 +0300

    create base_blog and _header in blog app

---
+++ blog/templatetags/header.py
@@ -0,0 +1,12 @@
+from django import template
+from django.db.models import Count, F
+from blog.models import Category
+
+register = template.Library()
+
+
+@register.inclusion_tag("templatetags_templates/show_categories.html")
+def show_categories():
+    categories = Category.objects.annotate(cnt=Count("get_category", filter=F("get_category__is_published"))).\
+        filter(cnt__gt=0)
+    return {"categories": categories, }
