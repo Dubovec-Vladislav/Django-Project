fec6e09a0cdbe5f493f7eeccb95be553566508c1
Vladislav <dubovecvlad0@gmail.com>
2023-01-12 10:12:01 +0300

    connection blog app

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', main, name="home"),
     path('about/', about, name="about"),
     path('services/', include('services.urls')),
+    path('blog/', include('blog.urls')),
 ]
