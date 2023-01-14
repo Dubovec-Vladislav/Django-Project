017d316cdfa40c10dfcc0e2570a91e9e9817ceca
Vladislav <dubovecvlad0@gmail.com>
2023-01-14 14:26:49 +0300

    update single_post template

--- blog/templatetags/header.py
+++ blog/templatetags/header.py
@@ -5,7 +5,7 @@ from blog.models import Category
 register = template.Library()
 
 
-@register.inclusion_tag("templatetags_templates/show_categories.html")
+@register.inclusion_tag("templatetags_templates/show_categories_tpl.html")
 def show_categories():
     categories = Category.objects.annotate(cnt=Count("get_category", filter=F("get_category__is_published"))).\
         filter(cnt__gt=0)
