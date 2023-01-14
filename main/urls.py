8d9720b880c997f7440acbd551b6a1f3946c0a4d
Vladislav <dubovecvlad0@gmail.com>
2023-01-14 15:14:46 +0300

    connection blog on the home page

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', main, name="home"),
+    path('', Home.as_view(), name="home"),
     path('about/', about, name="about"),
     path('services/', include('services.urls')),
     path('blog/', include('blog.urls')),
--- main/views.py
+++ main/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from blog.models import Post
+
+
+class Home(ListView):
+    model = Post
+    template_name = 'main/home.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Classic Blog Design'
+        return context
 
 
 def main(request):
