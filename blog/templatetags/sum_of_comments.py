3b1c9260365f1294ac87d81b91cfc61145f035e1
Vladislav <dubovecvlad0@gmail.com>
2023-01-17 17:26:24 +0300

    update sum of comments func and add likes counter

--- blog/templatetags/sum_of_comments.py
+++ blog/templatetags/sum_of_comments.py
@@ -5,5 +5,9 @@ register = template.Library()
 
 @register.inclusion_tag('templatetags_templates/sum_of_comments_tpl.html')
 def show_sum_of_comments(post):
-    comments = post.get_comments.count
-    return {"comments": comments}
+    comments = post.get_comments.count()
+    reply_comments = 0
+    for comment in post.get_comments.all():
+        reply_comments += comment.get_parent_comments.count()
+    number_of_comments = comments + reply_comments
+    return {"number_of_comments": number_of_comments, }
